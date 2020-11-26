605498eb149235924ed78bf592c7b3514d2c3c5f
deivisjl <deivisjl@gmail.com>
2020-11-26 16:23:01 -0600

    Agregando react-fontawesome

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Chat from './components/Chat/Chat'
 import ProtectedRoute from './components/Router/ProtectedRoute'
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSmile, faImage } from '@fortawesome/free-regular-svg-icons'
+import { faSpinner, faEllipsisV, faUserPlus, faSignOutAlt, faTrash, faCaretDown, faUpload, faTimes, faBell } from '@fortawesome/free-solid-svg-icons'
+library.add(faSmile, faImage, faSpinner, faEllipsisV, faUserPlus, faSignOutAlt, faTrash, faCaretDown, faUpload, faTimes, faBell)
 
 function App() {
   return (
--- src/components/Chat/components/Navbar/Navbar.js
+++ src/components/Chat/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
 import './Navbar.scss'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const Navbar = () =>{
     const user = useSelector(state => state.authReducer.user)
@@ -9,8 +10,9 @@ const Navbar = () =>{
         <div id="navbar" className='card-shadow'>
             <h2>Chat.io</h2>
             <div id='profile-menu'>
-                <img src='' alt='Avatar'/>
+                <img width="40px" height="40px" src={user.avatar} alt='Avatar'/>
                 <p>{user.firstName} {user.lastName}</p>
+                <FontAwesomeIcon icon='caret-down' className='fa-icon'/>
             </div>
         </div>
     )
