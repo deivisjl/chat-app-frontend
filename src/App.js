181bd6609e333c797794d123757eef0a9ada59bb
deivisjl <deivisjl@gmail.com>
2020-11-26 09:50:12 -0600

    Agregando react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react'
 import './App.css';
+
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import Chat from './components/Chat/Chat'
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Chat app</h1>
-    </div>
+    <Router>
+        <div className="App">
+            <Switch>
+              <Route exact path='/' component={Chat}></Route>
+              <Route path='/login' component={Login}></Route>
+              <Route path='/register' component={Register}></Route>
+              <Route render={() => <h1>404 page not found</h1>}/>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
