ab083cd7ed936a91279db985de7bc105ab2c79af
deivisjl <deivisjl@gmail.com>
2020-11-26 15:08:06 -0600

    Estilos de la navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 import Chat from './components/Chat/Chat'
 
+import ProtectedRoute from './components/Router/ProtectedRoute'
+
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
@@ -12,7 +14,7 @@ function App() {
     <Router>
         <div className="App">
             <Switch>
-              <Route exact path='/' component={Chat}></Route>
+              <ProtectedRoute exact path='/' component={Chat}></ProtectedRoute>
               <Route path='/login' component={Login}></Route>
               <Route path='/register' component={Register}></Route>
               <Route render={() => <h1>404 page not found</h1>}/>
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
+import Navbar from './components/Navbar/Navbar'
+
+import './Chat.scss'
 
 const Chat = ()=>{
 
     const user = useSelector(state => state.authReducer.user)
 
     return (
-        <div>
-            <h1>Chat screen</h1>
-            <p>Welcome, {user.firstName} </p>
+        <div id='chat-container'>
+            <Navbar/>
+            <div id='chat-wrap'>
+
+            </div>
         </div>
     )
 }
