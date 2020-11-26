6d82071eddbc0ff0934e35ccbb99028b04c0bf01
deivisjl <deivisjl@gmail.com>
2020-11-26 11:41:45 -0600

    Agregando axios a las peticiones

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import loginImage from '../../assets/images/male.svg'
 import {Link} from 'react-router-dom'
+//import axios from 'axios'
 
 import './Auth.scss'
+import AuthService from '../../services/authService'
 
 const Login = ()=>{
+    const [email,setEmail] = useState('john.doe@gmail.com')
+    const [password,setPassword] = useState('secret')
+
+
+    const submitForm = (e) => {
+        e.preventDefault()
+
+        AuthService.login({email, password})
+            .then(res =>{
+
+            })
+            .catch(error =>{
+
+            })
+        /* axios.post('http://127.0.0.1:8081/login',{email, password})
+            .then(res=>{
+                console.log(res)
+            })
+            .catch(error =>{
+                console.log(error)
+            }) */
+    }
+
     return (
         <div id='auth-container'>
              <div id='auth-card'>
@@ -14,13 +39,23 @@ const Login = ()=>{
                     </div> 
                     <div id='form-section'>
                         <h2>Welcome back</h2>
-                        <form>
+                        <form onSubmit={submitForm}>
                             <div>
                                 <div className='input-field mb-1'>
-                                    <input placeholder='Email'/>
+                                    <input 
+                                    onChange={e => setEmail(e.target.value)}
+                                    value={email}
+                                    require='required'
+                                    type='text'
+                                    placeholder='Email'/>
                                 </div>
                                 <div className='input-field mb-2'>
-                                    <input placeholder='Password'/>
+                                    <input
+                                    onChange={e => setPassword(e.target.value)} 
+                                    value={password}
+                                    require='required'
+                                    type='password'
+                                    placeholder='Password'/>
                                 </div>
                                 <button>LOGIN</button>
                             </div>
